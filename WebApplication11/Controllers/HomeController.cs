c2ce7e3bbc4fa244974cbd0fadf1e68ef9eafd7b
gokulakrishnas <gokulakrishna.sureshkumar@gadaget.in>
2018-06-11 10:38:53 +0530

    new

--- WebApplication11/Controllers/HomeController.cs
+++ WebApplication11/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace WebApplication11.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact pag
 
             return View();
         }
