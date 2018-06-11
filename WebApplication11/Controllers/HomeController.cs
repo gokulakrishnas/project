155e225d66a8d2c001c9bd2f6eb67a1f7632d5b7
gokulakrishnas <gokulakrishna.sureshkumar@gadaget.in>
2018-06-11 10:26:53 +0530

    new code

--- WebApplication11/Controllers/HomeController.cs
+++ WebApplication11/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication11.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = 
+            ViewBag.Message = "";
 
             return View();
         }
